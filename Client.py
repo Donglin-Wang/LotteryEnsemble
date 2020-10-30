3cb2b10ac51e4458c7f25bf711f932e3a31b8dd1
bwubete <bwubete@ciena.com>
2020-10-29 22:09:12 -0400

    (feature): First trial of LTN integration with FL

--- Client.py
+++ Client.py
@@ -5,133 +5,281 @@ import torchvision as tv
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as tf
+import torch.nn.init as init
+from torch.utils.data import DataLoader, Dataset
 
-class Client:
-    def __init__(self, args, data_loader):
-        
-        if args.dataset == "mnist": from archs.mnist import mlp
-        else:
-            print("You did not enter the name of a supported dataset")
-            print("Supported datasets: {}, {}".format('"CIFAR10"', '"MNIST"'))
-            exit()
-        
-        if args.arch == 'mlp': self.model = mlp.MLP()
-        else:
-            print("You did not enter the name of a supported architecture for this dataset")
-            print("Supported datasets: {}, {}".format('"CIFAR10"', '"MNIST"'))
-            exit()
-        
-        assert self.model, "Something went wrong and the model cannot be initialized"
-        
-        self.log_freq = args.log_freq
-        self.init_model_state = copy.deepcopy(self.model.state_dict())
-        self.train_loader = data_loader
-        self.client_epoch = args.client_epoch
-        self.prune_iterations = args.prune_iterations
-        self.prune_percent = args.prune_percent
-        self.mask = self.init_mask(self.model)
-        
-        # TODO: implement the following initilizations
-        # self.rtarget = ???  (See LotterFL Page 4 Algo 1) 
-        # self.acc_target = ??? (See LotterFL Page 4 Algo 1)
-        
-    def client_update_loop(self, global_state, global_init_weight):
-       self.model.load_state_dict(global_state)
-       for i in range(self.prune_iterations):
-           self.train()
-           self.mask = self.get_prune_mask(prune_percent, self.model)
-          
-           
-    def train(self):
-        loss_function = nn.CrossEntropyLoss()
-        opt = optim.Adam(self.model.parameters(), lr=self.lr)
-        error = []
-        print("Model is starting to train...")
-        for epoch in range(self.client_epoch):
-            total_loss = 0.0
-            for i, data in enumerate(self.train_loader, 0):
-                # Getting inputs and predictions
-                inputs, labels = data
-                opt.zero_grad()
-                pred = self.model(inputs)
-                # Gradient descent
-                loss = loss_function(pred, labels)
+
+class DatasetSplit(Dataset):
+    """An abstract Dataset class wrapped around Pytorch Dataset class.
+    """
+
+    def __init__(self, dataset, idxs):
+        self.dataset = dataset
+        self.idxs = [int(i) for i in idxs]
+
+    def __len__(self):
+        return len(self.idxs)
+
+    def __getitem__(self, item):
+        image, label = self.dataset[self.idxs[item]]
+        return torch.tensor(image), torch.tensor(label)
+
+
+
+class Client(object):
+    def __init__(self, args, dataset, idxs, logger, accThreshold, pruneRate):
+        self.args = args
+        self.logger = logger
+        self.trainloader, self.validloader, self.testloader = self.train_val_test(
+            dataset, list(idxs))
+        self.device = 'cuda' if args.gpu else 'cpu'
+        # Default criterion set to NLL loss function
+
+        self.criterion = nn.NLLLoss().to(self.device)
+
+        self.model = None
+        self.mask = None
+        self.initial_state_dict = None
+        self.accThreshold = accThreshold
+        self.pruneRate = pruneRate
+
+    def train_val_test(self, dataset, idxs):
+        """
+        Returns train, validation and test dataloaders for a given dataset
+        and user indexes.
+        """
+        # split indexes for train, validation, and test (80, 10, 10)
+        idxs_train = idxs[:int(0.8*len(idxs))]
+        idxs_val = idxs[int(0.8*len(idxs)):int(0.9*len(idxs))]
+        idxs_test = idxs[int(0.9*len(idxs)):]
+
+        trainloader = DataLoader(DatasetSplit(dataset, idxs_train),
+                                 batch_size=self.args.local_bs, shuffle=True)
+        validloader = DataLoader(DatasetSplit(dataset, idxs_val),
+                                 batch_size=int(len(idxs_val)/10), shuffle=False)
+        testloader = DataLoader(DatasetSplit(dataset, idxs_test),
+                                batch_size=int(len(idxs_test)/10), shuffle=False)
+        return trainloader, validloader, testloader
+
+    def update_weights(self, global_round):
+        # Set mode to train model
+        assert self.model, "Model needs to be set, please call prune_by_percentile first"
+        model = self.model
+        model.train()
+        epoch_loss = []
+
+        # Set optimizer for the local updates
+        if self.args.optimizer == 'sgd':
+            optimizer = torch.optim.SGD(model.parameters(), lr=self.args.lr,
+                                        momentum=0.5)
+        elif self.args.optimizer == 'adam':
+            optimizer = torch.optim.Adam(model.parameters(), lr=self.args.lr,
+                                         weight_decay=1e-4)
+
+        for iter in range(self.args.local_ep):
+            batch_loss = []
+            for batch_idx, (images, labels) in enumerate(self.trainloader):
+                images, labels = images.to(self.device), labels.to(self.device)
+
+                model.zero_grad()
+                log_probs = model(images)
+                loss = self.criterion(log_probs, labels)
                 loss.backward()
-                opt.step()
-                # Loss Calculation
-                total_loss += loss.item()
-                if i % self.log_freq == self.log_freq - 1:
-                   error.append(total_loss / self.log_freq)
-                   total_loss = 0.0
-        print("Finished Training")
-        return error
-    
-    def test():
-        return
-    
-    def init_mask(self, model):
-        
-        assert model, "init_mask() is called before the model is initialized"
-        
-        layer = 0
-        for name, param in model.named_parameters(): 
-            if 'weight' in name:
-                layer = layer + 1
-        mask = [None]* layer
-        
-        layer = 0
-        for name, param in model.named_parameters(): 
-            if 'weight' in name:
-                tensor = param.data.cpu().numpy()
-                mask[layer] = np.ones_like(tensor)
-                layer = layer + 1
-        layer = 0
-        
-        return mask
-    
-            
-    def prune():
-        return
-    
-    def set_to_init_weights():
-        return
-    def get_prune_mask(self, percent, model):
-        
-        assert model, "prune_by_percent() is called before the model is initialized"
-        
-        # Calculate percentile value
-        layer = 0
-        for name, param in model.named_parameters():
+                optimizer.step()
 
+                if self.args.verbose and (batch_idx % 10 == 0):
+                    print('| Global Round : {} | Local Epoch : {} | [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        global_round, iter, batch_idx * len(images),
+                        len(self.trainloader.dataset),
+                                            100. * batch_idx / len(self.trainloader), loss.item()))
+                self.logger.add_scalar('loss', loss.item())
+                batch_loss.append(loss.item())
+            epoch_loss.append(sum(batch_loss)/len(batch_loss))
+
+        return model.state_dict(), sum(epoch_loss) / len(epoch_loss)
+
+    def inference(self, model):
+        """ Returns the inference accuracy and loss.
+        """
+
+        model.eval()
+        loss, total, correct = 0.0, 0.0, 0.0
+
+        for batch_idx, (images, labels) in enumerate(self.testloader):
+            images, labels = images.to(self.device), labels.to(self.device)
+
+            # Inference
+            outputs = model(images)
+            batch_loss = self.criterion(outputs, labels)
+            loss += batch_loss.item()
+
+            # Prediction
+            _, pred_labels = torch.max(outputs, 1)
+            pred_labels = pred_labels.view(-1)
+            correct += torch.sum(torch.eq(pred_labels, labels)).item()
+            total += len(labels)
+
+        accuracy = correct/total
+        return accuracy, loss
+
+    def prune_by_percentile(self, percent, model, resample=False, reinit=False, **kwargs):
+        if self.model is None: # First iteration
+            self.initial_state_dict = copy.deepcopy(model.state_dict())
+            self.model = model
+
+        if self.mask is None: # First iteration
+            self.make_mask(self.model)
+
+        acc, loss = self.inference(self.model)
+
+        step = 0
+        # Calculate percentile value
+        for name, param in self.model.named_parameters():
             # We do not prune bias term
             if 'weight' in name:
                 tensor = param.data.cpu().numpy()
+                total_params = np.prod(tensor.shape)
+                nz_count = np.count_nonzero(tensor)
+                pRate = (total_params - nz_count) / total_params
                 alive = tensor[np.nonzero(tensor)] # flattened array of nonzero values
                 percentile_value = np.percentile(abs(alive), percent)
 
                 # Convert Tensors to numpy and calculate
                 weight_dev = param.device
-                new_mask = np.where(abs(tensor) < percentile_value, 0, mask[layer])
-                
+                if acc > self.accThreshold and pRate < self.pruneRate: # Only prune if acc good enough and prate is not as high as configured prune rate
+                    new_mask = np.where(abs(tensor) < percentile_value, 0, self.mask[step])
+                else:
+                    new_mask = self.mask[step]
                 # Apply new weight and mask
                 param.data = torch.from_numpy(tensor * new_mask).to(weight_dev)
-                mask[layer] = new_mask
-                layer += 1
-        layer = 0
-        
-        return mask
+                self.mask[step] = new_mask
+                step += 1
 
-if __name__ == "__main__":
-    from DataSource import get_data
-    # Creating an empty object to which we can add any attributes
-    args = type('', (), {})()
-    
-    args.dataset = 'mnist'
-    args.arch = 'mlp'
-    args.client_epoch = 100
-    args.prune_iterations = 10
-    args.prune_type = 'reinit'
-    args.prune_percent = 10
-    
-    client_loaders, test_loader = get_data(1, 'mnist')
-    client = Client(args, client_loaders[0])
+        if reinit:
+            self.model.apply(self.weight_init)
+            #LTH implementation prunes after this step
+            self.prune_by_percentile(percent, model)
+        else:
+            self.original_initialization()
+
+    def make_mask(self, model):
+        step = 0
+        for name, param in model.named_parameters():
+            if 'weight' in name:
+                step = step + 1
+        self.mask = [None] * step
+        step = 0
+        for name, param in model.named_parameters():
+            if 'weight' in name:
+                tensor = param.data.cpu().numpy()
+                self.mask[step] = np.ones_like(tensor)
+                step = step + 1
+        step = 0
+
+    def original_initialization(self):
+        assert self.model, "Model needs to be set, please call prune_by_percentile first"
+        step = 0
+        for name, param in self.model.named_parameters():
+            if "weight" in name:
+                weight_dev = param.device
+                param.data = torch.from_numpy(self.mask[step] * self.initial_state_dict[name].cpu().numpy()).to(weight_dev)
+                step = step + 1
+            if "bias" in name:
+                param.data = self.initial_state_dict[name]
+
+    def weight_init(self, m):
+        '''
+        Usage:
+            model = Model()
+            model.apply(weight_init)
+        '''
+        if isinstance(m, nn.Conv1d):
+            init.normal_(m.weight.data)
+            if m.bias is not None:
+                init.normal_(m.bias.data)
+        elif isinstance(m, nn.Conv2d):
+            init.xavier_normal_(m.weight.data)
+            if m.bias is not None:
+                init.normal_(m.bias.data)
+        elif isinstance(m, nn.Conv3d):
+            init.xavier_normal_(m.weight.data)
+            if m.bias is not None:
+                init.normal_(m.bias.data)
+        elif isinstance(m, nn.ConvTranspose1d):
+            init.normal_(m.weight.data)
+            if m.bias is not None:
+                init.normal_(m.bias.data)
+        elif isinstance(m, nn.ConvTranspose2d):
+            init.xavier_normal_(m.weight.data)
+            if m.bias is not None:
+                init.normal_(m.bias.data)
+        elif isinstance(m, nn.ConvTranspose3d):
+            init.xavier_normal_(m.weight.data)
+            if m.bias is not None:
+                init.normal_(m.bias.data)
+        elif isinstance(m, nn.BatchNorm1d):
+            init.normal_(m.weight.data, mean=1, std=0.02)
+            init.constant_(m.bias.data, 0)
+        elif isinstance(m, nn.BatchNorm2d):
+            init.normal_(m.weight.data, mean=1, std=0.02)
+            init.constant_(m.bias.data, 0)
+        elif isinstance(m, nn.BatchNorm3d):
+            init.normal_(m.weight.data, mean=1, std=0.02)
+            init.constant_(m.bias.data, 0)
+        elif isinstance(m, nn.Linear):
+            init.xavier_normal_(m.weight.data)
+            init.normal_(m.bias.data)
+        elif isinstance(m, nn.LSTM):
+            for param in m.parameters():
+                if len(param.shape) >= 2:
+                    init.orthogonal_(param.data)
+                else:
+                    init.normal_(param.data)
+        elif isinstance(m, nn.LSTMCell):
+            for param in m.parameters():
+                if len(param.shape) >= 2:
+                    init.orthogonal_(param.data)
+                else:
+                    init.normal_(param.data)
+        elif isinstance(m, nn.GRU):
+            for param in m.parameters():
+                if len(param.shape) >= 2:
+                    init.orthogonal_(param.data)
+                else:
+                    init.normal_(param.data)
+        elif isinstance(m, nn.GRUCell):
+            for param in m.parameters():
+                if len(param.shape) >= 2:
+                    init.orthogonal_(param.data)
+                else:
+                    init.normal_(param.data)
+
+
+def test_inference(args, model, test_dataset):
+    """ Returns the test accuracy and loss.
+    """
+
+    model.eval()
+    loss, total, correct = 0.0, 0.0, 0.0
+
+    device = 'cuda' if args.gpu else 'cpu'
+    criterion = nn.NLLLoss().to(device)
+    testloader = DataLoader(test_dataset, batch_size=128,
+                            shuffle=False)
+
+    for batch_idx, (images, labels) in enumerate(testloader):
+        images, labels = images.to(device), labels.to(device)
+
+        # Inference
+        outputs = model(images)
+        batch_loss = criterion(outputs, labels)
+        loss += batch_loss.item()
+
+        # Prediction
+        _, pred_labels = torch.max(outputs, 1)
+        pred_labels = pred_labels.view(-1)
+        correct += torch.sum(torch.eq(pred_labels, labels)).item()
+        total += len(labels)
+
+    accuracy = correct/total
+    return accuracy, loss
--- Server.py
+++ Server.py
@@ -1,12 +1,136 @@
-class Server():
-    def __init__(self, args):
-        
-        self.comm_rounds = args.comm_rounds
-        
-        # TODO: implement the following initilizations
-        # self.clients = ???
-        # self.init_weights = ???
-        # self.weights = ???
-        
-    def weight_init():
-        
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
+
+import os
+import copy
+import time
+import pickle
+import numpy as np
+from tqdm import tqdm
+
+import torch
+from tensorboardX import SummaryWriter
+
+from Client import Client, test_inference
+from options import args_parser
+
+from models import MLP, CNNMnist, CNNFashion_Mnist, CNNCifar
+from utils import get_dataset, average_weights, exp_details
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    # define paths
+    path_project = os.path.abspath('..')
+    logger = SummaryWriter('../logs')
+
+    args = args_parser()
+    exp_details(args)
+
+    if args.gpu:
+        torch.cuda.set_device(args.gpu)
+    device = 'cuda' if args.gpu else 'cpu'
+
+    # load dataset and user groups
+    train_dataset, test_dataset, user_groups = get_dataset(args)
+
+    # BUILD MODEL
+    if args.model == 'cnn':
+        # Convolutional neural netork
+        if args.dataset == 'mnist':
+            global_model = CNNMnist(args=args)
+        elif args.dataset == 'fmnist':
+            global_model = CNNFashion_Mnist(args=args)
+        elif args.dataset == 'cifar':
+            global_model = CNNCifar(args=args)
+
+    elif args.model == 'mlp':
+        # Multi-layer preceptron
+        img_size = train_dataset[0][0].shape
+        len_in = 1
+        for x in img_size:
+            len_in *= x
+            global_model = MLP(dim_in=len_in, dim_hidden=64,
+                               dim_out=args.num_classes)
+    else:
+        exit('Error: unrecognized model')
+
+    # Set the model to train and send it to device.
+    global_model.to(device)
+    global_model.train()
+    print(global_model)
+
+    # copy weights
+    global_weights = global_model.state_dict()
+
+    #init clients
+    clients = []
+    for i in range(args.num_users):
+        clients.append(Client(args = args, dataset = train_dataset, idxs=user_groups[i],  logger=logger, accThreshold=0.8, pruneRate=0.9))
+
+    # Training
+    train_loss, train_accuracy = [], []
+    val_acc_list, net_list = [], []
+    cv_loss, cv_acc = [], []
+    print_every = 2
+    val_loss_pre, counter = 0, 0
+
+    for epoch in tqdm(range(args.epochs)):
+        local_weights, local_losses = [], []
+        print(f'\n | Global Training Round : {epoch+1} |\n')
+
+        global_model.train()
+        m = max(int(args.frac * args.num_users), 1)
+        idxs_users = np.random.choice(range(args.num_users), m, replace=False)
+
+        for idx in idxs_users:
+            local_model = clients[idx]
+            local_model.prune_by_percentile(0.5, model=copy.deepcopy(global_model))
+            w, loss = local_model.update_weights(global_round=epoch)
+            local_weights.append(copy.deepcopy(w))
+            local_losses.append(copy.deepcopy(loss))
+
+        # update global weights
+        global_weights = average_weights(local_weights)
+
+        # update global weights
+        global_model.load_state_dict(global_weights)
+
+        loss_avg = sum(local_losses) / len(local_losses)
+        train_loss.append(loss_avg)
+
+        # Calculate avg training accuracy over all users at every epoch
+        list_acc, list_loss = [], []
+        global_model.eval()
+        for c in range(args.num_users):
+            local_model = clients[c]
+            acc, loss = local_model.inference(model=global_model)
+            list_acc.append(acc)
+            list_loss.append(loss)
+        train_accuracy.append(sum(list_acc)/len(list_acc))
+
+        # print global training loss after every 'i' rounds
+        if (epoch+1) % print_every == 0:
+            print(f' \nAvg Training Stats after {epoch+1} global rounds:')
+            print(f'Training Loss : {np.mean(np.array(train_loss))}')
+            print('Train Accuracy: {:.2f}% \n'.format(100*train_accuracy[-1]))
+
+    # Test inference after completion of training
+    test_acc, test_loss = test_inference(args, global_model, test_dataset)
+
+    print(f' \n Results after {args.epochs} global rounds of training:')
+    print("|---- Avg Train Accuracy: {:.2f}%".format(100*train_accuracy[-1]))
+    print("|---- Test Accuracy: {:.2f}%".format(100*test_acc))
+
+    # Saving the objects train_loss and train_accuracy:
+    file_name = '../save/objects/{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}].pkl'. \
+        format(args.dataset, args.model, args.epochs, args.frac, args.iid,
+               args.local_ep, args.local_bs)
+
+    with open(file_name, 'wb') as f:
+        pickle.dump([train_loss, train_accuracy], f)
+
+    print('\n Total Run Time: {0:0.4f}'.format(time.time()-start_time))
