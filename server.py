CODE: First draft of the averaging logic with minor bug fixes

server.py
@@ -36,16 +36,17 @@ class Server():
     def default_server_update(self):
         
         # For each client, 0 means no update and 1 means update
-        update_or_not = [0] * self.num_clients
+       
         
         # Recording the update and storing them in record
         for i in range(1, self.comm_rounds+1):
-            
+            update_or_not = [0] * self.num_clients
             # Randomly select a fraction of users to update
             num_selected_clients = max(int(self.frac * self.num_clients), 1)
             idx_list = np.random.choice(range(self.num_clients), num_selected_clients)
             for idx in idx_list:
                 update_or_not[idx] = 1
+            print(idx_list)
             
             print('-------------------------------------', flush=True)
             print(f'Communication Round #{i}', flush=True)
@@ -59,7 +60,7 @@ class Server():
                     self.client_models[i][j] = copy_model(self.clients[j].model, self.args.dataset, self.args.arch)
             
             models = self.client_models[i][idx_list]
-            self.global_models[i] = average_weights(models)
+            self.global_models[i] = average_weights(models, self.args.dataset, self.args.arch)
 
 # This is a dummy test to see if the server works
 if __name__ == '__main__':

test.py
@@ -183,58 +183,30 @@ def test6_copy_weights():
     print('After Copying')
     # print(list(new_mlp.named_parameters()))
     print(list(new_mlp.named_buffers()))
-    
-
-
-    
-    
-
-
-
-# Helper Methods
-
-# def average_weights(models):
-#     with torch.no_grad():
-#         weights = []
-#         for model in models:
-#             weights.append(dict(model.named_parameters()))
-        
-#         avg = copy.deepcopy(weights[0])
-#         for key in avg.keys():
-#             for i in range(1, len(weights)):
-#                 avg[key] += weights[i][key]
-#             avg[key] = torch.div(avg[key], len(weights))
-#     return avg
-
-
-# def copy_weights(target_model, source_state_dict):
-
-#     for name, param in target_model.named_parameters():
-#         if name in source_state_dict:
-            
-#             param.data.copy_(source_state_dict[name].data)
-
-# def gen_state_dict(model):
-#     new_state_dict = collections.OrderedDict()
-    
-#     for name, param in model.named_parameters():
-#         if name.endswith('_orig'):
-#             new_state_dict[name] = torch.zeros_like(param)
-            
-#     return new_state_dict
-
-# def count_zero_weights(model):
-#     layers, num_param = get_prune_params(model)
-#     num_zeros = 0
-#     for layer, _ in layers:
-#         num_zeros += torch.sum(layer.weight == 0.0)
-#     return num_zeros
-
-# def finalize_pruning(model):
-#     layers, num_param = get_prune_params(model)
-#     for layer, _ in layers:
-#         prune.remove(layer, 'weight')
-    
+ 
+def test7_average_weights():
+    def average_weights(models, dataset, arch, data_len):
+        new_model = create_model(dataset, arch)
+        num_models = len(models)
+        with torch.no_grad():
+            # Getting all the weights and masks from original models
+            weights, masks = [], []
+            for i in range(num_models):
+                weights.append(dict(models[i].named_parameters()))
+                masks.append(dict(models[i].named_buffers()))
+            # Averaging weights
+            for name, param in new_model.named_parameters():
+                for i in range(num_models):
+                    param.data.copy_(param.data + weights[i])
+                avg = torch.div(param.data, num_models)
+                param.data.copy_(avg)
+            # Averaging masks
+            for name, buffer in new_model.named_buffers():
+                for i in range(num_models):
+                    buffer.data.copy_(buffer.data + masks[i])
+                avg = torch.div(buffer.data, num_models)
+                buffer.data.copy_(avg)
+        return new_model
 
 if __name__ == '__main__':
     
@@ -251,6 +223,8 @@ if __name__ == '__main__':
     # test5_create_model()
     
     test6_copy_weights()
+    
+    test7_average_weights()
     
     
     

util.py
@@ -6,24 +6,41 @@ import torch.nn as nn
 import torch.optim as optim
 from sklearn import metrics as skmetrics
 
-
 from tqdm import tqdm
 from tabulate import tabulate
 import torch.nn.utils.prune as prune
 
 
-def average_weights(models):
+def average_weights(models, dataset, arch):
+    new_model = create_model(dataset, arch)
+    num_models = len(models)
     with torch.no_grad():
-        weights = []
-        for model in models:
-            weights.append(dict(model.named_parameters()))
-        
-        avg = copy.deepcopy(weights[0])
-        for key in avg.keys():
-            for i in range(1, len(weights)):
-                avg[key] += weights[i][key]
-            avg[key] = torch.div(avg[key], len(weights))
-    return avg
+        # Getting all the weights and masks from original models
+        weights, masks = [], []
+        for i in range(num_models):
+            weights.append(dict(models[i].named_parameters()))
+            masks.append(dict(models[i].named_buffers()))
+        # Averaging weights
+        for name, param in new_model.named_parameters():
+            for i in range(num_models):
+                param.data.copy_(param.data + weights[i][name])
+            avg = torch.div(param.data, num_models)
+            param.data.copy_(avg)
+        # Averaging masks
+        for name, buffer in new_model.named_buffers():
+            for i in range(num_models):
+                buffer.data.copy_(buffer.data + masks[i][name])
+            avg = torch.div(buffer.data, num_models)
+            
+            # Clipping all the values to [0.0, 1.0]. This might
+            # seems trivial, but if you don't do this, you will get an 
+            # error message saying that there's not parameters to prune.
+            # This has something to do with how pruning is handled internally
+            
+            avg = torch.clamp(avg, 0.0, 1.0)
+            avg = torch.round(avg)
+            buffer.data.copy_(avg)
+    return new_model
 
 
 
@@ -158,7 +175,7 @@ def evaluate(model, data_loader, verbose=True):
     if verbose:
         print('Evaluation Score: ')   
         print(tabulate(score, headers='keys', tablefmt='github'), flush=True)
-    
+    model.train()
     torch.enable_grad()
     return score
 