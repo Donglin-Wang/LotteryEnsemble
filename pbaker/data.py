c38852d20c4371d1959fb8ef2100269f0146adc3
Patrick <55208187+patgbaker@users.noreply.github.com>
2020-11-15 10:04:58 -0500

    CHORE: trim data for dev in data module rather than foreman

--- pbaker/data.py
+++ pbaker/data.py
@@ -7,6 +7,7 @@
 # TODO: extend to support non-IID
 def split_data(data, k):
     partition = []
+    data = (data[0][:5000], data[1][:5000])             #    !!!!!!!!!!!!! Chopped the data set size during development
     n = len(data[0])
     splits = [(n // k) * i for i in range(k)]
 
--- pbaker/foreman.py
+++ pbaker/foreman.py
@@ -6,6 +6,7 @@ from client_LotteryFL import Client_LotteryFL
 from server_FedAvg import Server_FedAvg
 from server_LotterFL import Server_LotteryFL
 
+
 class Foreman():
     def __init__(self, params):
         self.params = params
@@ -31,16 +32,16 @@ class Foreman():
                 self.clients.append(Client_FedAvg(k,
                                                   self.model_creator,
                                                   {key: params[key] for key in ('E', 'B', 'eta')},
-                                                  partition[k][0][:1000, :],
-                                                  partition[k][1][:1000]))
+                                                  partition[k][0],
+                                                  partition[k][1]))
             elif params['algo'] == 'LotteryFL':
                 self.clients.append(Client_LotteryFL(k,
                                                      self.model_creator,
                                                      self.initial_weights,
                                                      {key: params[key] for key in ('E', 'B', 'eta', 'acc_threshold',
                                                                                    'r_target', 'r_p')},
-                                                     partition[k][0][:1000, :],
-                                                     partition[k][1][:1000]))
+                                                     partition[k][0],
+                                                     partition[k][1]))
             else:
                 assert False, f"Unsupported argument for algo: {params['algo']}"
 
@@ -48,26 +49,26 @@ class Foreman():
         if params['algo'] == 'FedAvg':
             self.server = Server_FedAvg(self.clients, self.global_model,
                                         {key: params[key] for key in ('R', 'C', 'K')},
-                                        self.X_train[:1000, :], self.y_train[:1000])
+                                        self.X_train, self.y_train)
         elif params['algo'] == 'LotteryFL':
             self.server = Server_LotteryFL(self.clients, self.global_model,
                                            {key: params[key] for key in ('R', 'C', 'K')},
-                                           self.X_train[:1000, :], self.y_train[:1000])
+                                           self.X_train, self.y_train)
         else:
             self.server = Server_Genesis(self.clients, self.global_model,
                                          {key: params[key] for key in ('R', 'C', 'K')},
-                                         self.X_train[:1000, :], self.y_train[:1000])
+                                         self.X_train, self.y_train)
 
     def run(self):
         print("Foreman: run with initial weights")
-        self.global_model.evaluate(self.X_train[:1000, :], self.y_train[:1000])
+        self.global_model.evaluate(self.X_train, self.y_train)
 
         self.server.run()
         print("Foreman: run with trained global model")
         self.global_model.set_weights(self.server.get_weights())
-        self.global_model.evaluate(self.X_train[:1000, :], self.y_train[:1000])
+        self.global_model.evaluate(self.X_train, self.y_train)
 
         # we evaluate final client and server model performance at this point
         print("Foreman: rerun with initial weights")
         self.global_model.set_weights(self.initial_weights)
-        self.global_model.evaluate(self.X_train[:1000, :], self.y_train[:1000])
+        self.global_model.evaluate(self.X_train, self.y_train)
